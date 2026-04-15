0155b5df2539c0d92d14b4bf5c26938edf880eb8
Vivian Tuháčková <viviantuhackova@Vivian--MacBook-Air.local>
2026-04-15 14:06:01 +0200

    implementace interaktivni mapy Leaflet s vydejnimi misty

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>Moje semestrální práce - Tropico</title>
     <link rel="stylesheet" href="styl.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     </head>
 <body>
 
@@ -62,11 +64,45 @@
             </div>
         </div>
     </div>
+    
+    <div id="kontakt">
+        <h2 style="text-align: center;">Kde nás najdete a odkud dovážíme:</h2>
+        <div id="map" style="height: 500px; width: 90%; margin: 20px auto; border: 1px solid #ccc; border-radius: 8px;"></div>
+    </div>
 
     <div id="paticka">
         <hr>
         <p>&copy; 2026 TROPICO - Vivian Tuháčková</p>
     </div>
+    
+    <script>
+        var map = L.map('map').setView([49.8, 15.5], 7);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        L.marker([50.6607, 14.0325]).addTo(map)
+            .bindPopup('<b>Pobočka Ústí</b><br>Dovážíme z: Thajska');
+
+        L.marker([50.0878, 14.4205]).addTo(map)
+            .bindPopup('<b>Pobočka Praha</b><br>Dovážíme z: Vietnamu');
+
+        L.marker([49.1951, 16.6068]).addTo(map)
+            .bindPopup('<b>Pobočka Brno</b><br>Dovážíme z: Vietnamu');
+
+        L.marker([49.7475, 13.3776]).addTo(map)
+            .bindPopup('<b>Pobočka Plzeň</b><br>Dovážíme z: Thajska');
+
+        L.marker([49.8209, 18.2625]).addTo(map)
+            .bindPopup('<b>Pobočka Ostrava</b><br>Dovážíme z: Thajska');
+
+        L.marker([48.9745, 14.4743]).addTo(map)
+            .bindPopup('<b>Pobočka Č. Budějovice</b><br>Dovážíme z: Vietnamu');
+
+        setTimeout(function(){ map.invalidateSize(); }, 500);
+    </script>
 
 </body>
 </html>
